e41fb7c3c5b10d917a59b3d9712adc8d287f5398
fanger2791 <m.fang@mail.utoronto.ca>
2024-04-02 19:26:00 -0400

    cleaned 2022/2021 verstappen and hamilton data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,36 +9,89 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(readr)
+library(dplyr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Read in DataSets
+raw_ver_jeddah_2022 <- read_csv("data/raw_data/verstappen_laps_jeddah.csv")
+raw_ver_jeddah_2021 <- read_csv("data/raw_data/verstappen_laps_jeddah_2021.csv")
+raw_ham_jeddah_2022 <- read_csv("data/raw_data/hamilton_laps_jeddah.csv")
+raw_ham_jeddah_2021 <- read_csv("data/raw_data/hamilton_laps_jeddah_2021.csv")
+raw_lec_jeddah_2022 <- read_csv("data/raw_data/leclerc_laps_jeddah.csv")
+raw_lec_jeddah_2021 <- read_csv("data/raw_data/leclerc_laps_jeddah_2021.csv")
+
+
+#### Clean Verstappen Jeddah 2022 DataSet ####
+
+# Check structure
+str(raw_ver_jeddah_2022)
+# Convert time_sec to numeric
+raw_ver_jeddah_2022 <- raw_ver_jeddah_2022 %>%
+  mutate(time_sec = as.numeric(time_sec), 
+         season = as.factor(season))
+#Assuming laps over 100 seconds are due to errors or non-racing reasons (pitstops):
+raw_ver_jeddah_2022 <- filter(raw_ver_jeddah_2022, time_sec < 100)
+#Remove rows with missing values
+raw_ver_jeddah_2022 <- drop_na(raw_ver_jeddah_2022)
+raw_ver_jeddah_2022 <- raw_ver_jeddah_2022 %>%
+  rename(Driver = driver_id,`Position`= position, `Lap Time` = time, `Lap #`= lap, `LapTime in Seconds`= time_sec, `Season` = season)
+
+#### Clean Verstappen Jeddah 2021 DataSet ####
+
+# Check structure
+str(raw_ver_jeddah_2021)
+# Convert time_sec to numeric
+raw_ver_jeddah_2021 <- raw_ver_jeddah_2021 %>%
+  mutate(time_sec = as.numeric(time_sec), 
+         season = as.factor(season))
+#Assuming laps over 100 seconds are due to errors or non-racing reasons (pitstops):
+raw_ver_jeddah_2021 <- filter(raw_ver_jeddah_2021, time_sec < 100)
+#Remove rows with missing values
+raw_ver_jeddah_2021 <- drop_na(raw_ver_jeddah_2021)
+raw_ver_jeddah_2021 <- raw_ver_jeddah_2021 %>%
+  rename(Driver = driver_id,`Position`= position, `Lap Time` = time, `Lap #`= lap, `LapTime in Seconds`= time_sec, `Season` = season)
+
+#### Clean Hamilton Jeddah 2022 DataSet ####
+
+# Check structure
+str(raw_ham_jeddah_2022)
+# Convert time_sec to numeric
+raw_ham_jeddah_2022 <- raw_ham_jeddah_2022 %>%
+  mutate(time_sec = as.numeric(time_sec), 
+         season = as.factor(season))
+#Assuming laps over 100 seconds are due to errors or non-racing reasons (pitstops):
+raw_ham_jeddah_2022 <- filter(raw_ham_jeddah_2022, time_sec < 100)
+#Remove rows with missing values
+raw_ham_jeddah_2022 <- drop_na(raw_ham_jeddah_2022)
+raw_ham_jeddah_2022 <- raw_ham_jeddah_2022 %>%
+  rename(Driver = driver_id,`Position`= position, `Lap Time` = time, `Lap #`= lap, `LapTime in Seconds`= time_sec, `Season` = season)
+
+#### Clean Hamilton Jeddah 2021 DataSet ####
+
+# Check structure
+str(raw_ham_jeddah_2021)
+# Convert time_sec to numeric
+raw_ham_jeddah_2021 <- raw_ham_jeddah_2021 %>%
+  mutate(time_sec = as.numeric(time_sec), 
+         season = as.factor(season))
+#Assuming laps over 100 seconds are due to errors or non-racing reasons (pitstops):
+raw_ham_jeddah_2021 <- filter(raw_ham_jeddah_2021, time_sec < 100)
+#Remove rows with missing values
+raw_ham_jeddah_2021 <- drop_na(raw_ham_jeddah_2021)
+raw_ham_jeddah_2021 <- raw_ham_jeddah_2021 %>%
+  rename(Driver = driver_id,`Position`= position, `Lap Time` = time, `Lap #`= lap, `LapTime in Seconds`= time_sec, `Season` = season)
+
+#### Clean Leclerc Jeddah 2022 DataSet ####
+
+
+
+
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(raw_ver_jeddah_2022, "data/analysis_data/verstappen_laps_jeddah_2022_cleaned.csv")
+write_csv(raw_ver_jeddah_2021, "data/analysis_data/verstappen_laps_jeddah_2021_cleaned.csv")
+write_csv(raw_ham_jeddah_2022, "data/analysis_data/hamilton_laps_jeddah_2022_cleaned.csv")
+write_csv(raw_ham_jeddah_2021, "data/analysis_data/hamilton_laps_jeddah_2021_cleaned.csv")
+
