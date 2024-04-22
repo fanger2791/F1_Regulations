4889888ed2a4ba3c60dc48dd6c955edabfba0f3d
fanger2791 <m.fang@mail.utoronto.ca>
2024-04-22 15:16:11 -0400

    updated preamble

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,12 +1,10 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates F1 Laptime and Speed Data
+# Author: Michael Fang
+# Date: 22 April 2024
+# Contact: m.fang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites: Not Applicable
 
 #### Workspace setup ####
 library(tidyverse)
